eead4e0374608c6369dedbc4c5a71ac146fd6068
kamimura <kamimura@macbook-pro.local>
2014-08-10 18:24:12 +0900

    Initial Commit

---
+++ string/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  string
+//
+//  Created by kamimura on 8/10/14.
+//  Copyright (c) 2014 kamimura. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
